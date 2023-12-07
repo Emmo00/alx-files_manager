e1414ee2dab09897374319fec6e23c3014e15107
Emmanuel Nwafor@ubuntuVM <nwaforemmanuel005@gmail.com>
2023-12-07 01:03:21 +0100

    rename

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
-import UserController from '../controllers/UserController';
+import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 
 const router = express.Router();
@@ -9,12 +9,12 @@ router.get('/status', AppController.getStatus);
 
 router.get('/stats', AppController.getStats);
 
-router.post('/users', UserController.postNew);
+router.post('/users', UsersController.postNew);
 
 router.get('/connect', AuthController.getConnect);
 
 router.get('/disconnect', AuthController.getDisconnect);
 
-router.get('/users/me', UserController.getMe);
+router.get('/users/me', UsersController.getMe);
 
 export default router;
